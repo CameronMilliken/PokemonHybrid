54b77cafb71e0fd9ed7d6a226c2211448084fa79
CameronMilliken <cameron.milliken@gmail.com>
2019-01-01 10:07:18 -0700

    Initial Commit

---
+++ PokeDex23OBJC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PokeDex23OBJC
+//
+//  Created by Cameron Milliken on 1/1/19.
+//  Copyright © 2019 Cameron Milliken. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PokeDex23OBJC/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  PokeDex23OBJC
+//
+//  Created by Cameron Milliken on 1/1/19.
+//  Copyright © 2019 Cameron Milliken. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
